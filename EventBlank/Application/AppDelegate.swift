89ff62b240def1efd3e49ed99b933c41f0a7484f
Alexandr Pavlov <alexandr.pavlov@outlook.com>
2018-11-20 18:14:29 +0200

    Deleted Main.storyboard

--- EventBlank/Application/AppDelegate.swift
+++ EventBlank/Application/AppDelegate.swift
@@ -4,9 +4,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var rootController: UINavigationController? {
+        return self.window!.rootViewController as? UINavigationController
+    }
+    
+    var assembler: AppAssembler? {
+        return DefaultAppAssembler.shared
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        setUpWindow()
+        
         return true
+    }
+    
+    private func setUpWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController()
+        window?.makeKeyAndVisible()
     }
 }
